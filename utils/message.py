b21f1ea7aece6a54d25918df3681a689e4d1ff2f
Bence Faludi <bence@6wunderkinder.com>
2017-02-10 17:49:46 +0100

    message generation

--- utils/message.py
+++ utils/message.py
@@ -14,28 +14,66 @@ class Message(object):
         self.collection = self.set_collection()
         self.time = 1454514088
 
+    def get_parameters(self):
+        for i in range(random.randint(0,4)):
+            p = Parameter()
+            p.name = 'Key.{}'.format(i)
+            p.value = 'Value.{}'.format(random.randint(1,100000))
+            yield p
+
     def get_payload(self):
         p = Payload()
         p.at = int(time.time())
         p.event = 'Event.{}'.format(random.randint(10000,99999))
         p.nr = random.randint(1,1000)
-        p.user_id = 'User.{}'.format(random.randint(1,99000000))
+        if random.randint(1,2) == 1:
+            params = []
+        else:
+            params = list(self.get_parameters())
+        if random.randint(1,3) != 1:
+            p.timezone = '{}{}:00'.format(['+','-'][random.randint(0,1)], random.randint(0,12))
+        if random.randint(1,3) != 1:
+            p.tenant_id = 'Tenant.{}'.format(random.randint(1,1000))
+        if random.randint(1,3) != 1:
+            p.user_id = 'User.{}'.format(random.randint(1,99000000))
         if random.randint(1,2) == 1:
             p.ip = '{}.{}.{}.{}'.format(random.randint(1,255), random.randint(1,255),
                                         random.randint(1,255), random.randint(1,255))
+        else:
+            param = Parameter()
+            param.name = 'RemoteIPFromServer'
+            param.value = 'true'
+            params.append(param)
+        if params:
+            p.parameters.extend(params)
         return p
 
     def set_collection(self):
         c = Collection()
-        c.env = random.randint(0,11)
         c.device_id = hashlib.sha256(str(uuid.uuid4())).hexdigest()
         c.client_id = hashlib.md5(str(random.randint(1,1000000))).hexdigest()[:20]
         c.system_version = '{}.{}'.format(random.randint(1,5), random.randint(1,50))
         c.product_version = '{}.{}'.format(random.randint(1,5), random.randint(1,50))
-        c.session = hashlib.md5("{device_id}:{client_id}:{system_version}:{product_version}".format(
+        c.env = random.randint(0,11)  
+        c.session = hashlib.md5("{device_id}:{client_id}:{system_version}:{product_version}:{env}".format(
             device_id=c.device_id, client_id=c.client_id, system_version=c.system_version,
-            product_version=c.product_version)).hexdigest()
-        c.system = ['OSX','Windows','iOS','Android'][random.randint(0,3)]
+            product_version=c.product_version, env=c.env)).hexdigest()
+        if random.randint(1,3) != 1:
+            c.device_make = ['Apple','Microsoft','Lenovo','IBM'][random.randint(0,3)]
+        if random.randint(1,3) != 1:
+            c.device_model = ['iPhone 6','iPhone 7','Macbook 12,1','Microsoft Surface Pro'][random.randint(0,3)]
+        if random.randint(1,3) != 1:
+            c.system = ['OSX','Windows','iOS','Android'][random.randint(0,3)]
+        if random.randint(1,3) != 1:
+            c.system_language = ['hu_HU','de_DE','en_US','en_GB'][random.randint(0,3)]
+        if random.randint(1,3) != 1:
+            c.browser = ['Chrome','Edge','IE','Safari','Firefox'][random.randint(0,4)]
+        if random.randint(1,3) != 1:
+            c.browser_version = '{}.{}'.format(random.randint(1,5), random.randint(1,50))
+        if random.randint(1,3) != 1:
+            c.product_git_hash = c.session
+        if random.randint(1,3) != 1:
+            c.product_language = ['hu_HU','de_DE','en_US','en_GB'][random.randint(0,3)]
         number = random.randint(1,25) \
             if self.random_payload \
             else 1
